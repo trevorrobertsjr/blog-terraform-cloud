1229b676100e4584261170d05f9b6dba341e9cb2
Trevor Roberts Jr <trevor.roberts.jr@gmail.com>
2024-02-19 20:24:59 -0500

    removed all references to shared config files

--- variables.tf
+++ variables.tf
@@ -3,14 +3,14 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "tfc_aws_dynamic_credentials" {
-  description = "Object containing AWS dynamic credentials configuration"
-  type = object({
-    default = object({
-      shared_config_file = string
-    })
-    aliases = map(object({
-      shared_config_file = string
-    }))
-  })
-}
+# variable "tfc_aws_dynamic_credentials" {
+#   description = "Object containing AWS dynamic credentials configuration"
+#   type = object({
+#     default = object({
+#       shared_config_file = string
+#     })
+#     aliases = map(object({
+#       shared_config_file = string
+#     }))
+#   })
+# }
