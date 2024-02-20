df281ae5f3568be6eef386a40ba84d47759ab1f6
Trevor Roberts Jr <trevor.roberts.jr@gmail.com>
2024-02-19 19:10:12 -0500

    initialer commit

--- main.tf
+++ main.tf
@@ -1,31 +1,47 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_lambda_function" "example_lambda" {
+  function_name = "go_CloudFront_invalidate"
+  handler       = "bootstrap"  # Update this based on your runtime and entry point.
+  role          = aws_iam_role.example_lambda_role.arn  # Ensure you have a role with appropriate permissions.
+  runtime       = "provided.al2023"  # Replace with the correct runtime identifier.
+  s3_bucket     = "blog-terraform-input-artifacts"
+  s3_key        = "goInvalidateCacheNoRPC.zip"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+  # Optionally, specify the S3 object version if you want to use a specific version of the ZIP file
+  # s3_object_version = "your-object-version"
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+  source_code_hash = filebase64sha256(goInvalidateCacheNoRPC.zip")  # For local ZIP file checksum. Update the path as necessary.
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_iam_role" "example_lambda_role" {
+  name = "example_lambda_role"
 
-  tags = {
-    Name = var.instance_name
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid = ""
+      },
+    ]
+  })
 }
+
+# Attach a policy to the IAM role to grant necessary permissions for Lambda
+resource "aws_iam_policy_attachment" "lambda_basic_execution_cloudfront_codepipeline" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole", 
+    "arn:aws:iam::aws:policy/CloudFrontFullAccess",
+    "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
+  ])
+  roles      = [aws_iam_role.example_lambda_role.name]
+  policy_arn = each.value
+}
+
